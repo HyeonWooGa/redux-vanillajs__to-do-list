5af42802b892378619b33b72fdcf90e9c41ae971
HyeonWooGa <yeonwoopark22@gmail.com>
2022-09-01 01:45:17 +0900

    💻 추가기능 수정 - 해결 리스트 두번 입력

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ store.subscribe(() => console.log(store.getState()));
 
 const paintToDos = () => {
   const toDos = store.getState();
+  ul.innerHTML = "";
   toDos.forEach((toDo) => {
     const li = document.createElement("li");
     li.id = toDo.id;
