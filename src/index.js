79b49f1f52f676d0979479961b57409c3ec62882
HyeonWooGa <yeonwoopark22@gmail.com>
2022-09-01 01:21:20 +0900

    💻 To Do List 추가기능

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
+import { createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, action.text];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  const li = document.createElement("li");
+  store.getState().forEach((toDo) => {
+    li.innerText = toDo;
+    ul.append(li);
+  });
+});
 
 const onSubmit = (event) => {
   event.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo);
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
 form.addEventListener("submit", onSubmit);
